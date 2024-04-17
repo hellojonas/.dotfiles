b72b648316d0c54656cca660f30f7bae78576a29
Hello Jonas <hello.jonas@2ibi.com>
2024-04-17 10:20:40 +0200

    Add vim-sleuth and dap config

--- nvim/lua/user/plugins/init.lua
+++ nvim/lua/user/plugins/init.lua
@@ -1,19 +1,20 @@
 return {
-  'tpope/vim-repeat',
-  'tpope/vim-surround',
-  {
-     'windwp/nvim-autopairs',
-      event = "InsertEnter",
-      config = true
-  },
-  {
-      'numToStr/Comment.nvim',
-      opts = {},
-      lazy = false,
-  },
-  {
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-      build = "make install_jsregexp"
-  },
+    'tpope/vim-repeat',
+    'tpope/vim-surround',
+    'tpope/vim-sleuth',
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+    },
+    {
+        'numToStr/Comment.nvim',
+        opts = {},
+        lazy = false,
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp"
+    },
 }
