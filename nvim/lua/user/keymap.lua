fa39edb1a5de4765541f5135e1581a9447c2508c
Hello Jonas <hello.jonas@2ibi.com>
2024-05-23 10:42:23 +0200

    Add oil.nvim & configs

--- nvim/lua/user/keymap.lua
+++ nvim/lua/user/keymap.lua
@@ -4,6 +4,7 @@ end
 
 nmap('<Esc>', '<cmd>nohlsearch<CR>')
 nmap('<leader>db', '<cmd>bw!<CR>')
+nmap('<leader>dba', '<cmd>%bw!<CR>')
 nmap('<leader>fe', '<cmd>Ex<CR>')
 nmap('<leader>r', '<cmd>source $MYVIMRC<CR>')
 
@@ -11,3 +12,8 @@ nmap('<C-k>', '<C-w>k')
 nmap('<C-j>', '<C-w>j')
 nmap('<C-h>', '<C-w>h')
 nmap('<C-l>', '<C-w>l')
+
+nmap('gh', '<cmd>diffget //2<CR>')
+nmap('gl', '<cmd>diffget //3<CR>')
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
--- nvim/lua/user/plugins/nvim-cmp.lua
+++ nvim/lua/user/plugins/nvim-cmp.lua
@@ -76,7 +76,12 @@ return {
         'hrsh7th/cmp-buffer',
         'hrsh7th/cmp-path',
         'hrsh7th/cmp-cmdline',
-        'onsails/lspkind.nvim'
+        'onsails/lspkind.nvim',
+        {
+            "L3MON4D3/LuaSnip",
+            version = "v2.*",
+            build = "make install_jsregexp"
+        },
     },
     config = setup
 }
