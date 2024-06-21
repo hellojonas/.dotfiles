1f8fd86cb497067961957f6ae3c252946e962328
Hello Jonas <hello.jonas@2ibi.com>
2024-06-21 11:40:30 +0200

    disable telescop startup

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,11 +10,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     group = highlight_group,
     pattern = '*',
 })
-
-local telescope_on_startup_group = vim.api.nvim_create_augroup('TelescopOnStartup', { clear = true })
-vim.api.nvim_create_autocmd('VimEnter', {
-    group = telescope_on_startup_group,
-    callback = function(e)
-        require('telescope.builtin').find_files()
-    end
-})
