1f631fc9f87ba6184d18bbac502b0383db65430f
Hello Jonas <hellojonashp@gmail.com>
2025-02-19 16:37:24 +0200

    completion accept on enter

--- nvim/lua/user/plugins/blink-cmp.lua
+++ nvim/lua/user/plugins/blink-cmp.lua
@@ -6,7 +6,16 @@ return {
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
-        keymap = { preset = 'default' },
+        keymap = { preset = 'enter' },
+        completion = {
+            list = {
+                selection = {
+                    preselect = false,
+                    auto_insert = false,
+                }
+            }
+        },
+
 
         signature = {
             enabled = true,
